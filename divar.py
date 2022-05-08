ab4a7bae65638be2804c6865a01b319569803fa5
shimaf <fshima02@gmail.com>
2022-05-08 14:45:20 +0430

    get all children

--- divar.py
+++ divar.py
@@ -12,11 +12,17 @@ def BackslashEscape(x):
     return x
 
 def get_all_data():
-    response = requests.get(URL).text
-    json_str = re.search(r"window\.__PRELOADED_STATE__\s+=\s+\"(.*)\";", response)[1]
-    json_str = BackslashEscape(json_str)
-    json_data = json.loads(json_str)
-    return json_data
+    # response = requests.get(URL).text
+   
+    # get url from json file 
+    f = open('a.json').read()
+    response = json.loads(f)
+
+    # json_str = re.search(r"window\.__PRELOADED_STATE__\s+=\s+\"(.*)\";", response)[1]
+    # json_str = BackslashEscape(json_str)
+    # json_data = json.loads(json_str)
+    # return json_data
+    return response
 
 def get_cities():
     json_data = get_all_data()
@@ -28,5 +34,21 @@ def get_categories():
     categories = json_data['search']['rootCat']
     return categories
 
+
+def child_to_array(array):
+    while True:
+        for category in array:
+            if 'children' in category:
+                if len(category['children']) != 0:
+                    array += [*category['children']]
+                del category['children']
+    
+        if not any('children' in x for x in array):
+            break
+    return array
+
+
 def search_in_categories(category_name, categories):
-    x = []
+    categories = [get_categories()]
+    all_subcategories = child_to_array(categories)
+    return all_subcategories
